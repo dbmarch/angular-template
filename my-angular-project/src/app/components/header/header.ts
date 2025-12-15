d72c7077339e923f38912696e3e855a7f20ffdbc
Dave March <dbmarch@gmail.com>
2025-12-14 22:20:05 -0500

    Tweak buttons

--- my-angular-project/src/app/components/header/header.ts
+++ my-angular-project/src/app/components/header/header.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+
+  OnClick() {
+    console.log('Header icon clicked');
+  }
 
 }
